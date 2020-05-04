5d95a01dd6bd3288c5cc6cbb5a60dd241cde9e64
Martin Brajer <martin.brajer@seznam.cz>
2020-05-04 13:50:28 +0200

    Refactoring

--- CardAssembler_Data/CardAssembler_Definitions.py
+++ CardAssembler_Data/CardAssembler_Definitions.py
@@ -2,8 +2,8 @@
 """
 Supplemental script to gimp plug-in CardCreator.
 
-Introduces Card class which can read xml file and
-return layout list, which is used by the main script.
+Introduces Blueprint class which can read xml file and
+produce layout list, which is used by the main script.
 """
 # ---IMPORTS---
 import xml.etree.ElementTree as ET
@@ -26,9 +26,9 @@ def main():
 
 
 class Blueprint(object):
-    """ Card information handling class.
+    """ Blueprint information handling class.
 
-    Can read xml file and return layout list,
+    Can read xml file and produce layout list,
     which is used by the main script.
     """
 
@@ -36,7 +36,7 @@ class Blueprint(object):
         self.data = self._load_data(filePath)
 
     def _load_data(self, filePath):
-        """ Load filepath -> xml file into dictionary tree. """
+        """ Load blueprint (xml file) into dictionary tree. """
         root = ET.parse(filePath).getroot()
         return self.xml2dict(root)
 
@@ -49,17 +49,16 @@ class Blueprint(object):
 
             # If there is no text (just another child) tail
             # is still there ("         \n").
-            # "item.text" == None <=> "<item></item>".
+            # Second: "item.text" == None <=> "<item></item>".
             if (text is not None) and (text.strip().replace("\n", "")):
-                # ElementTree unescapes newline so I'm reverting back.
+                # ElementTree unescapes newline so we're reverting back.
                 text = text.replace('\\n', '\n')
-                # Merge same-key entries.
                 if key in newDict:
                     text = newDict[key] + ', ' + text
-                # Parse if requested.
                 if child.attrib:
                     if 'parse' in child.attrib:
                         text = self._parse(text, child.attrib['parse'])
+
                 newDict[key] = text
 
             # Go down the level.
@@ -87,14 +86,16 @@ class Blueprint(object):
     def generate_layout_dict(self, startBy):
         """ Generate layout given starting position.
 
-        Starting position keys are sorted alphabetically
-        (name them accordingly).
+        Starting position children are sorted
+        alphabetically (name them accordingly).
         """
         return self._stepIn({}, startBy)
 
     def _stepIn(self, layout, thisStep):
-        """ Browse data guided by 'next' key
-        and fill missing values. """
+        """ Browse data guided by 'next' tag.
+
+        Does not overwrite (first in stays).
+        """
         nextSteps = []
         for key, value in self._nextDataset(thisStep).items():
             # Next is not written into layout.
@@ -108,7 +109,7 @@ class Blueprint(object):
                     layout[key] = {}
                 self._stepIn(layout[key], thisStep + ' ' + key)
 
-            # Keys having values from higher levels are NOT changed.
+            # Keys having values from previous levels are NOT changed.
             elif key not in layout:
                 layout[key] = value
 
--- card-assembler.py
+++ card-assembler.py
@@ -16,13 +16,9 @@ import sys
 
 
 # ---FUNCTIONS---
-def data_folder():
-    """ Find Data folder.
-
-    It is set as default value
-    in GF.register function.
-    """
-    dataFolder = os.path.expanduser('~')  # .replace("\\", "/")
+def default_data_folder():
+    """ Gimp in user folder. """
+    dataFolder = os.path.expanduser('~')
     # Rest of the Gimp plug-in path contains gimp version
     # and therefore can change. Any folder can be chosen.
     dataFolder += '/AppData/Roaming/GIMP/'
@@ -30,23 +26,17 @@ def data_folder():
 
 
 def card_creator(dataFolder, xmlFile, cardIDs, save):
-    """Registered function CardCreator, creates new card image
-    from predefined values. Requires two arguments, the paths to
-    the card definitions and chosen card ID.
-    """
+    """ Registered function by GF.register. I.e. Main(). """
     # This weird import is forced by Gimp running this script through
     # eval(...) function inside its installation folder and direct
     # import from different folder raises 'access denied' error.
-    dataFolder += '/'
     sys.path.append(dataFolder)
-    try:
-        import cardassembler_definitions
-    except ImportError, err:
-        raise ImportError('{} in folder {}'.format(str(err), dataFolder))
+    import cardassembler_definitions
 
     toolbox = Toolbox(dataFolder)
-    toolbox.blueprint = cardassembler_definitions.Blueprint(
-        dataFolder + xmlFile)
+    toolbox.load_blueprint(
+        xmlFile,
+        blueprintClass=cardassembler_definitions.Blueprint)
 
     if not cardIDs:
         raise ValueError('No card IDs inserted!')
@@ -60,9 +50,7 @@ def card_creator(dataFolder, xmlFile, cardIDs, save):
 
 # ---CLASSES---
 class Toolbox(object):
-    """ Blueprint to image manipulation tool.
-
-    Image build information is saved in an xml file.
+    """ Blueprint-to-image manipulation tool.
 
     This class offers means for creating common card components
     (i.e. text, icons). Then completes the image and optionally
@@ -70,149 +58,173 @@ class Toolbox(object):
     """
 
     def __init__(self, dataFolder):
-        """ Init.
-
-        Parameter 'dataFolder' is a location of the xml files.
-        """
         self.blueprint = None
-        self.dataFolder = dataFolder
+        self.dataFolder = dataFolder + '/'
 
-        # Gimp image objects
-        self.image = None  # Code output will be here.
-        self.data_images = {}  # {name: Gimp image object}
+        self.gimpImage = None
+        self.gimpImageImport = {}  # {name: Gimp image object}
 
-        # Image components this script can handle.
-        self.component_type = {
-            'image': self._component_image,
-            'monochrome': self._component_monochrome,
-            'import_layer_load': self._component_import_layer_load,
-            'import_layer': self._component_import_layer,
-            'group': self._component_group,
-            'text': self._component_text,
+        # Specifies how a command dict is interpreted.
+        self.commandLib = {
+            'image': self._command_image,
+            'monochrome': self._command_monochrome,
+            'import_layer_load': self._command_import_layer_load,
+            'import_layer': self._command_import_layer,
+            'group': self._command_group,
+            'text': self._command_text,
         }
 
+    def load_blueprint(self, xmlFile, blueprintClass):
+        """ Initialize Blueprint class from supplemental file. """
+        self.blueprint = blueprintClass(self.dataFolder + xmlFile)
+
     def create_image(self, cardID):
-        """ Blueprint > layout information si utilized here. """
+        """ Blueprint to image.
+
+        Layout consists of commands, which are called alphabetically by name.
+        """
         if self.blueprint is None:
-            raise Exception('Blueprint must be initialized first!')
+            raise AttributeError('Blueprint must be initialized first!')
 
         layout = self.blueprint.generate_layout_dict(cardID)
-        for layerName in sorted(layout.keys()):
-            layerLayout = layout[layerName]
-            if 'componentType' in layerLayout:
-                componentType = layerLayout['componentType']
-                if componentType in self.component_type:
-                    self.component_type[componentType](layerLayout)
-                else:
-                    raise KeyError(
-                        'Unknown componentType "{}" of layer "{}"'.format(
-                            componentType, layerName))
-            else:
-                raise KeyError(
-                    'Layer "{}" is missing componentType tag.'.format(layerName))
+        for commandName in sorted(layout.keys()):
+            self._command_interpreter(layout[commandName], commandName)
 
-    def _component_image(self, layout):
-        """ Create new image. This is the first call in a layout sequence.
+    def _command_interpreter(self, command, commandName):
+        """ Forward command to appropriate method. """
+        if 'commandType' not in command:
+            raise KeyError(
+                'Command "{}" is missing commandType tag.'.format(
+                    commandName))
+        commandType = command['commandType']
+
+        if commandType not in self.commandLib:
+            raise KeyError(
+                'Unknown commandType "{}" of command "{}"'.format(
+                    commandType, commandName))
+
+        self.commandLib[commandType](command)
+
+    def _command_image(self, command):
+        """ Create new image. Needed for layer creation.
 
         Parameters: size(tuple), [name(string)].
         """
         self.image = GF.pdb.gimp_image_new(
-            layout['size'][0], layout['size'][1], GF.RGB)
+            command['size'][0], command['size'][1], GF.RGB)
         GF.pdb.gimp_image_set_filename(
-            self.image, layout.get('name', 'Card Assembler Image'))
+            self.image, command.get('name', 'Card Assembler Image'))
 
-    def _component_monochrome(self, layout):
-        """ One-colour-filled-layer. Allows masks.
+    def _command_monochrome(self, command):
+        """ One-colour-filled-layer.
 
         Parameters: size(tuple), color(string),
             [name(string)], [position(tuple)],
             [addToPosition(int)].
         """
-        layer = GF.pdb.gimp_layer_new(self.image,
-                                      layout['size'][0], layout['size'][1],
-                                      GF.RGB, layout.get(
-                                          'name', 'Monochrome'),
-                                      100, GF.LAYER_MODE_NORMAL)
-        self.image.add_layer(layer, layout.get('addToPosition', 0))
-        if 'position' in layout:
-            GF.pdb.gimp_layer_set_offsets(layer, *layout['position'])
-        GF.pdb.gimp_context_set_foreground(layout['color'])
+        if self.image is None:
+            raise AttributeError('Image to add the layer to not found.')
+
+        layer = GF.pdb.gimp_layer_new(
+            self.image,
+            command['size'][0], command['size'][1],
+            GF.RGB,
+            command.get('name', 'Monochrome'),
+            100,
+            GF.LAYER_MODE_NORMAL)
+        self.image.add_layer(layer, command.get('addToPosition', 0))
+        if 'position' in command:
+            GF.pdb.gimp_layer_set_offsets(layer, *command['position'])
+        GF.pdb.gimp_context_set_foreground(command['color'])
         GF.pdb.gimp_drawable_edit_bucket_fill(layer, 0, 0, 0)
 
-    def _component_import_layer_load(self, layout):
+    def _command_import_layer_load(self, command):
         """ Load new data image.
 
         Must be in Data folder. Filename is specified in xml file.
         Name parameter is used in xml file to reference
         the imported file. That's why it's not optional parameter.
+
         Parameters: filename(string), name(string).
         """
-        filepath = self.dataFolder + layout['filename']
-        self.data_images[layout['name']] = GF.pdb.gimp_file_load(
+        filepath = self.dataFolder + command['filename']
+        self.gimpImageImport[command['name']] = GF.pdb.gimp_file_load(
             filepath, filepath)
 
-    def _component_import_layer(self, layout):
+    def _command_import_layer(self, command):
         """ Copy layer from a data image.
 
         Parameters: targetFile(string), targetLayer(string),
             [addToPosition(int)], [name(string)], [position(tuple)].
         """
+        if self.image is None:
+            raise AttributeError('Image to add the layer to not found.')
+
         oldLayer = GF.pdb.gimp_image_get_layer_by_name(
-            self.data_images[layout['targetFile']], layout['targetLayer'])
+            self.gimpImageImport[command['targetFile']], command['targetLayer'])
         newLayer = GF.pdb.gimp_layer_new_from_drawable(
             oldLayer, self.image)
-        self.image.add_layer(newLayer, layout.get('addToPosition', 0))
-        newLayer.name = layout.get('name', layout['targetLayer'])
+        self.image.add_layer(newLayer, command.get('addToPosition', 0))
+        newLayer.name = command.get('name', command['targetLayer'])
         GF.pdb.gimp_layer_set_offsets(
-            newLayer, *layout.get('position', (0, 0)))
+            newLayer, *command.get('position', (0, 0)))
 
-    def _component_group(self, layout):
+    def _command_group(self, command):
         """ Create new layer group.
 
-        To fill next layers in, their 'addToPosition'
-        parameter must be set to -1.
+        To fill next layers in, set its 'addToPosition' parameter to -1.
+
         Parameters: [addToPosition(int)], [name(string)].
         """
-        layerGroup = GF.pdb.gimp_layer_group_new(self.image)
-        self.image.add_layer(layerGroup, layout.get('addToPosition', 0))
-        layerGroup.name = layout.get('name', 'Group')
+        if self.image is None:
+            raise AttributeError('Image to add the layer to not found.')
 
-    def _component_text(self, layout):
+        layerGroup = GF.pdb.gimp_layer_group_new(self.image)
+        self.image.add_layer(layerGroup, command.get('addToPosition', 0))
+        layerGroup.name = command.get('name', 'Group')
+
+    def _command_text(self, command):
         """ Text layer.
 
         If Size is not filled in, 'dynamic' mode is used.
         Justification values: left(0), right(1), center(2), fill(3).
+
         Parameters: text(string), font(string), fontSize(int),
             [textScale(float)], [addToPosition(int)], [name(string)],
             [color(string)], [size(tuple)], [lineSpacing(float)],
             [letterSpacing(float)], [justification(int)], [position(tuple)].
         """
-        fontsize = layout['fontSize'] * layout.get('textScale', 1)
+        if self.image is None:
+            raise AttributeError('Image to add the layer to not found.')
+
+        fontsize = command['fontSize'] * command.get('textScale', 1)
         textLayer = GF.pdb.gimp_text_layer_new(
-            self.image, layout['text'],
-            layout['font'], fontsize, 0)
-        self.image.add_layer(textLayer, layout.get('addToPosition', 0))
-        textLayer.name = layout.get('name', 'Text Layer')
+            self.image,
+            command['text'],
+            command['font'],
+            fontsize, 0)
+        self.image.add_layer(textLayer, command.get('addToPosition', 0))
+        textLayer.name = command.get('name', 'Text Layer')
         GF.pdb.gimp_text_layer_set_color(
-            textLayer, layout.get('color', '#000000'))
-        if 'size' in layout:
+            textLayer, command.get('color', '#000000'))
+        if 'size' in command:
             GF.pdb.gimp_text_layer_resize(
-                textLayer, *layout['size'])
+                textLayer, *command['size'])
         GF.pdb.gimp_text_layer_set_line_spacing(
-            textLayer, layout.get('lineSpacing', 0))
+            textLayer, command.get('lineSpacing', 0))
         GF.pdb.gimp_text_layer_set_letter_spacing(
-            textLayer, layout.get('letterSpacing', 0))
+            textLayer, command.get('letterSpacing', 0))
         GF.pdb.gimp_text_layer_set_justification(
-            textLayer, layout.get('justification', 0))
+            textLayer, command.get('justification', 0))
         GF.pdb.gimp_layer_set_offsets(
-            textLayer, *layout.get('position', (0, 0)))
+            textLayer, *command.get('position', (0, 0)))
 
     def display_image(self):
         """ Display the created image. """
         display = GF.pdb.gimp_display_new(self.image)
 
     def save_image(self):
-        """ X """
+        """ Save the image as <image.name>.xcf into 'Saved images' subfolder. """
         directory = self.dataFolder + 'Saved images/'
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -232,7 +244,7 @@ GF.register(
     "Card Assembler",  # Menu Entry (label).
     "",  # Image Type - no image required (imagetypes).
     [  # (params).
-        (GF.PF_DIRNAME, "dataFolder", "Data folder:", data_folder()),
+        (GF.PF_DIRNAME, "dataFolder", "Data folder:", default_data_folder()),
         (GF.PF_STRING, "xmlFile", "XML file:", "Blueprint.xml"),
         (GF.PF_TEXT, "cardIDs", "Card IDs:", ""),
         (GF.PF_BOOL, "save", "Save:", False),
